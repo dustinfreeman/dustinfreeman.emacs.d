6d6d6312baa3233ed19b3c413b483ba3b123c881
Dustin Freeman <dustin.freeman@gmail.com>
2018-09-12 23:50:23 -0400

    Fix typo in .gitignore and add init.el

---
+++ init.el
@@ -0,0 +1,42 @@
+;; Dustin Freeman's .emacs.d/init.el
+;; Created: Aug 21, 2018
+
+(load-theme 'misterioso)
+
+;; Melpa install
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; I hate tabs!
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (omnisharp csv-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; I'd love to have the bash shell hosting emacs, and emacs, to
+;; share history. I'd love to. This may do that.
+(savehist-mode 1)
